a759957e4334f59515787a5f0e1200895559a01b
ronycastroc <castroroony@gmail.com>
2022-10-18 20:17:48 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { PORT } from "./configs/constants.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ src/database/db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+import { DATABASE_URL } from "../configs/constants..js";
+
+const { Pool } = pg;
+
+const connection = new Pool ({
+    connectionString: DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+export { connection };
