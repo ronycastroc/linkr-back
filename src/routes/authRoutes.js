912be5ea74a549ed518d36d3d7683c87b8a8db1b
Victormarzo <victor.marzo.monteiro@gmail.com>
2022-10-20 15:46:52 -0300

    algumas correções

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { signUp } from "../controllers/authControllers.js";
-import {postLink,getLinks} from "../controllers/timelineControllers.js"
+
 const router = express.Router();
 
 router.post("/auth/signup", signUp);
-router.post("/timeline", postLink);
-router.get("/timeline", getLinks)
 
 export default router;
