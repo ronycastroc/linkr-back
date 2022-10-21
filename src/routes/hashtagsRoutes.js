65be580909f23504eec7ec9efe3118b25bfd1c9c
Kássia <kassia_guterres@outlook.com>
2022-10-21 11:47:07 -0300

    bug fix

--- src/routes/hashtagsRoutes.js
+++ src/routes/hashtagsRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getHashtagTrending, getHashtagPosts } from "../controllers/hashtagsControllers.js";
+import { validateLoggedUser } from "../middlewares/userAuthMiddleware.js";
 
 const router = express.Router();
 router.get("/trending", getHashtagTrending); 
-router.get("/hashtag/:hashtag", getHashtagPosts);//validateLoggedUser
+router.get("/hashtag/:hashtag", validateLoggedUser, getHashtagPosts);//validateLoggedUser
 
 export default router;
