d878740e9b4d074383c8a3b8aff8a4dd12abe10e
Yan Amaral <yan@pop-os.localdomain>
2022-10-22 12:31:14 -0300

    fix: bug many likes per user

--- src/controllers/timelineControllers.js
+++ src/controllers/timelineControllers.js
@@ -1,47 +1,53 @@
 import { connection } from "../database/db.js";
 import urlMetadata from "url-metadata";
-import * as timelineRepository from "../repositories/timelineRepository.js"
+import * as timelineRepository from "../repositories/timelineRepository.js";
 import * as postRepository from "../repositories/postRepository.js";
 import * as likeRepository from "../repositories/likeRepository.js";
 
-const postLink=async(req,res)=>{
-    const {url,id}=req.body;
-    const urlMetadatas= urlMetadata;
-    let metadatas;
-    let description,image,title;
-    let {text}=req.body;
-    if(!text){
-        text=null;
-    }
-    try {
-    metadatas =  await urlMetadatas(url)
-    } catch (error) {
-        console.log(error);
-    }
-   description=metadatas.description;
-   image=metadatas.image;
-   title=metadatas.title;
-   try {
-    await timelineRepository.insertPost(id,text,url,description,image,title);
+const postLink = async (req, res) => {
+  const { url, id } = req.body;
+  const urlMetadatas = urlMetadata;
+  let metadatas;
+  let description, image, title;
+  let { text } = req.body;
+  if (!text) {
+    text = null;
+  }
+  try {
+    metadatas = await urlMetadatas(url);
+  } catch (error) {
+    console.log(error);
+  }
+  description = metadatas.description;
+  image = metadatas.image;
+  title = metadatas.title;
+  try {
+    await timelineRepository.insertPost(
+      id,
+      text,
+      url,
+      description,
+      image,
+      title
+    );
     res.sendStatus(201);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
-const getLinks = async (req,res)=>{
-    
-    try {
-        const urls = await timelineRepository.listPosts()
-        res.send(urls)
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-}
+const getLinks = async (req, res) => {
+  try {
+    const urls = await timelineRepository.listPosts();
+    res.send(urls);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 const erasePost = async (req, res) => {
   const { postId } = req.params;
-  const userId = res.loclas.userId;
+  const userId = res.locals.userId;
   try {
     const existingPost = await postRepository.getPost(postId);
     if (existingPost.rowCount === 0) {
